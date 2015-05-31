ba4bf6a82356accde75d0e11a44ff6e62faf591b
erkanerol <erkanerol92@gmail.com>
2015-05-31 23:05:52 +0300

    license,report and poster

--- src/com/boun/file/FileOperations.java
+++ src/com/boun/file/FileOperations.java
@@ -1,3 +1,19 @@
+/**
+ 	This file is part of Simple Peer to Peer Digital Currency Project.
+
+    It is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    It is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with it.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.boun.file;
 
 import java.io.BufferedReader;
@@ -63,8 +79,8 @@ public class FileOperations {
 		
 		createADirectory("/resources");
 		
-		createADirectory(CreateGenesis.GENESIS_DIRECTORY)
-		;
+		createADirectory(CreateGenesis.GENESIS_DIRECTORY);
+		
 		createADirectory(CreateGenesis.GENESIS_KEYS_DIRECTORY);
 		
 		createADirectory(CreateGenesis.PUBLIC_KEYS_DIRECTORY);
